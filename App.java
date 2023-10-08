ce3c5cce83a1d7f62577277372d2552a5b09833d
Seamus Doherty <114188110+SeamusDoherty1@users.noreply.github.com>
2023-10-08 16:38:05 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,30 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int num = 0;
+        int repeatOne = 0;
+        int avgNum = 0;
+        int iNum = 0;
+        for(int i = 1; i <= 200; i = i + 1 ){
+            iNum = i;
+            while(repeatOne < 2){
+                if(iNum % 2 == 0){
+                   iNum = iNum / 2 ;
+                }
+                else{
+                    iNum = (iNum * 3) + 1;
+                }
+
+                if(iNum == 1){
+                    repeatOne = repeatOne + 1;
+                }
+                num++;
+            }
+            if(avgNum < num){
+                avgNum = num;
+            }
+            num = 0;
+            repeatOne = 0;
+        }
+        System.out.println("the Max iterations is: " + avgNum);
+    }
+}
