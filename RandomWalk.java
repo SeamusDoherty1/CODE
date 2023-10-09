f8838f52ff9baff48206706964282081a977f3d4
Seamus Doherty <114188110+SeamusDoherty1@users.noreply.github.com>
2023-10-09 17:52:49 -0400

    Add files via upload

---
+++ RandomWalk.java
@@ -0,0 +1,30 @@
+public class RandomWalk {
+    public static void main(String[] args) throws Exception {
+        double distance = 7.0;
+        int steps = 0;
+        double avg = 1.0;
+        int greatestAmount = 0;
+        for(int i = 1; i <= 50; i = i + 1 ){
+            double place = 3.5;
+            while(place >= 0.0 && place <= 7.0){
+                if( Math.random() < .5){
+                    place =  place - 1.0;
+                }
+                else{
+                    place = place + 1.0;
+                }
+                steps = steps + 1;
+            }
+            avg = steps + avg;
+            if(greatestAmount < steps){
+                greatestAmount = steps;
+            }
+            place = 3.5;
+            steps = 0;
+        }
+        avg = avg / 50;
+        System.out.println("The average amount of steps is " + avg);
+        System.out.println("The greatest amount of steps is " + greatestAmount);
+
+    }
+}
